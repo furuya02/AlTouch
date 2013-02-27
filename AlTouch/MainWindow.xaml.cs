62ef77a2afdbf6bb449b259df4b8fe3168a4c9ae
furuya02 <sin@sapporoworks.ne.jp>
2013-02-28 05:13:28 +0900

    2013.02.28.0513

--- AlTouch/MainWindow.xaml.cs
+++ AlTouch/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace AlTouch {
@@ -6,14 +7,60 @@ namespace AlTouch {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window{
+        readonly NotifyIcon _notifyIcon; //タスクトレイに格納
         private Hook _hook;
+
         public MainWindow() {
             InitializeComponent();
 
             var screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
             var screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
 
-            _hook = new Hook(listBoxLog, screenWidth, screenHeight);
+            _hook = new Hook(ListBoxLog, screenWidth, screenHeight);
+
+            //タスクトレイアイコンの初期化
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Text = "AlTouch マウスでタッチ操作";
+            _notifyIcon.Icon = new System.Drawing.Icon("AlTouch.ico");
+
+            //タスクトレイへの表示
+            _notifyIcon.Visible = true;
+
+            //メニュー「終了」追加
+            var menuStrip = new ContextMenuStrip();
+            var exitItem = new ToolStripMenuItem();
+            exitItem.Text = "終了";
+            menuStrip.Items.Add(exitItem);
+            exitItem.Click += ExitItemClick;
+            _notifyIcon.ContextMenuStrip = menuStrip;
+            _notifyIcon.MouseClick += NotifyIconMouseClick;
         }
+
+        private void NotifyIconMouseClick(object sender, MouseEventArgs e) {
+            try {
+                if (e.Button == MouseButtons.Left) {
+                    //ウィンドウを可視化
+                    Visibility = Visibility.Visible;
+                    WindowState = WindowState.Normal;
+                }
+            } catch { }
+        }
+
+
+        //終了メニューのイベントハンドラ
+        private void ExitItemClick(object sender, EventArgs e) {
+            try {
+                _notifyIcon.Dispose();
+                System.Windows.Application.Current.Shutdown();
+            } catch { }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            try {
+                e.Cancel = true;
+                Visibility = Visibility.Collapsed;
+            } catch { }
+        }
+
     }
 }
